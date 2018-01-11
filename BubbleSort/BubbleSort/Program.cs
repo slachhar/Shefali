9fc2ff2d6a298c2bb4bb4eced8cca0a22527ab69
unknown <shefali.lachhar@gmail.com>
2018-01-11 23:52:08 +0530

    First commit

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = {5,1,14,12,9 };
+
+            for(int iteration=1; iteration< intArray.Length; iteration++)
+            {
+                for(int number=0;number<intArray.Length-1;number++)
+                {
+                    if (intArray[number] > intArray[number + 1])
+                    {
+                        int temp = intArray[number];
+                        intArray[number] = intArray[number + 1];
+                        intArray[number + 1] = temp;
+                    }
+                }
+                Console.WriteLine(string.Join(",",intArray));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BubbleSort/MergeSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 14, 33, 27, 10, 35, 19, 42, 44, 23 };
+           
+        }
+
+        static int[] CreateArray(int[] intArray)
+        {
+
+        }
+    }
+}
