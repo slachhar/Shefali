32d94f3897c8a1da789f6481cb3dd4baab72232f
unknown <shefali.lachhar@gmail.com>
2018-03-16 11:23:30 +0530

    Add existing file

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverringVsMethodShadowing
+{ 
+
+    class A
+    {
+
+        public virtual void CallMethod() { Method(); }
+        protected virtual void Method() { Console.WriteLine("A"); }
+    }
+
+    class B : A
+    {
+
+        protected override void Method() { Console.WriteLine("B"); }
+    }
+
+    class C : B
+    {
+
+        protected new virtual void Method() { Console.WriteLine("C"); }
+    }
+
+    class D : C
+    {
+
+        protected override void Method() { Console.WriteLine("D"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C obj = new D();
+            obj.CallMethod();
+            Console.ReadLine();
+        }
+    }
+}
